367aa740519131e0b4a2383d6869cb01fca7b724
Willian Inocencio <willian.i.alves@hotmail.com>
2022-10-20 21:33:33 -0300

    hello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Unisantos!!
+          Hello!!
         </p>
         <a
           className="App-link"
