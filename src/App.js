6d5d26ee0c58528fe906c747be546942ca857cfc
Willian Inocencio <willian.i.alves@hotmail.com>
2022-10-20 22:12:50 -0300

    hello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello!!
+          Hello FATEC!!
         </p>
         <a
           className="App-link"
